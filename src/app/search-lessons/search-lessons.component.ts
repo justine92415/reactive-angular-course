0afca882eca8cad31acdc0f7b9a14be6188cc02d
佳政 <JiaZheng@EMP2303475.local>
2023-12-03 18:55:57 +0800

    finish ch.5

--- src/app/search-lessons/search-lessons.component.ts
+++ src/app/search-lessons/search-lessons.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, inject} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {
@@ -15,6 +15,7 @@ import {
 } from 'rxjs/operators';
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
+import { CoursesService } from '../services/courses.service';
 
 
 @Component({
@@ -24,14 +25,29 @@ import {Lesson} from '../model/lesson';
 })
 export class SearchLessonsComponent implements OnInit {
 
+  searchResults$: Observable<Lesson[]>;
+
+  activeLesson: Lesson;
+
+  coursesService = inject(CoursesService);
+
   constructor() {
-
-
   }
 
   ngOnInit() {
 
+  }
 
+  onSearch(search: string) {
+    this.searchResults$ = this.coursesService.searchLessons(search)
+  }
+
+  openLesson(lesson: Lesson) {
+    this.activeLesson = lesson;
+  }
+
+  onBackToSearch() {
+    this.activeLesson = null;
   }
 
 }
